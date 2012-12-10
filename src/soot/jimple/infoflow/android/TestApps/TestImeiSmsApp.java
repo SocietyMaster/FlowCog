2ae25541feac49b28bb406d6e160918be1f823dc
jakob.kontor <jakob.kontor@1aa7bb90-9ff5-4f69-b167-f7964f9e8525>
2012-12-10 01:18:07 +0000

    small change

--- src/soot/jimple/infoflow/android/TestApps/TestImeiSmsApp.java
+++ src/soot/jimple/infoflow/android/TestApps/TestImeiSmsApp.java
@@ -49,10 +49,10 @@ public class TestImeiSmsApp {
 			app.setSinkConfigFileLocation("SinkConfig.txt");
 			app.setSourceConfigFileLocation("SourceConfig.txt");
 			//The command line to execute soot-dex to transform apk file to jimple files till -d
-			command = "cmd /C C:/glassfish3/jdk/jre/bin/javaw.exe -Xmx1024m -Dfile.encoding=Cp1252 -classpath C:/Users/Spikey/workspace/soot-dex/bin;C:/Users/Spikey/workspace/soot/classes;C:/Users/Spikey/workspace/jasmin/classes;C:/Users/Spikey/workspace/jasmin/libs/java_cup.jar;C:/Users/Spikey/workspace/soot/libs/polyglot.jar;C:/Users/Spikey/workspace/soot/libs/AXMLPrinter2.jar;C:/Users/Spikey/workspace/heros/bin;C:/Users/Spikey/workspace/heros/guava-13.0.jar;C:/Users/Spikey/workspace/soot-dex/baksmali-1.3.2.jar soot.Main -android-jars C:/Users/Spikey/Desktop/platforms -d";
+			command = "C:/glassfish3/jdk/jre/bin/javaw.exe -Xmx1024m -Dfile.encoding=Cp1252 -classpath C:/Users/Spikey/workspace/soot-dex/bin;C:/Users/Spikey/workspace/soot/classes;C:/Users/Spikey/workspace/jasmin/classes;C:/Users/Spikey/workspace/jasmin/libs/java_cup.jar;C:/Users/Spikey/workspace/soot/libs/polyglot.jar;C:/Users/Spikey/workspace/soot/libs/AXMLPrinter2.jar;C:/Users/Spikey/workspace/heros/bin;C:/Users/Spikey/workspace/heros/guava-13.0.jar;C:/Users/Spikey/workspace/soot-dex/baksmali-1.3.2.jar soot.Main -android-jars C:/Users/Spikey/Desktop/platforms -d";
 			String middleLine = " -allow-phantom-refs -src-prec apk -process-dir ";
 			String endLine = " -output-format jimple";
-			command = command.concat(" ").concat(jimpleOutput).concat(middleLine).concat(apkFileLocation).concat(endLine);
+			command = "cmd /C ".concat(command).concat(" ").concat(jimpleOutput).concat(middleLine).concat(apkFileLocation).concat(endLine);
 			generate = false; //set to true after you have entered your correct command line
 		}
 		
