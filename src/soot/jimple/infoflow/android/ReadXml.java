a52457b3699af8146aa3197cf7a6474e4d29fddd
jakob.kontor <jakob.kontor@1aa7bb90-9ff5-4f69-b167-f7964f9e8525>
2013-02-13 08:36:23 +0000

    changed the search for onClick Methods

--- src/soot/jimple/infoflow/android/ReadXml.java
+++ src/soot/jimple/infoflow/android/ReadXml.java
@@ -21,15 +21,13 @@ import android.content.res.AXmlResourceParser;
 
 public class ReadXml {
 	private Map<String, List<String>> additionalEntryPoints = new HashMap<String, List<String>>();
-	
+
 	public void generateAndroidAppPermissionMap(String apk) {
-		
-		
+
 		File apkF = new File(apk);
-		
 
 		if (!apkF.exists())
-			throw new RuntimeException("file '" + apk + "' does not exist!"); 
+			throw new RuntimeException("file '" + apk + "' does not exist!");
 
 		// get xmlFiles
 		InputStream xmlFile = null;
@@ -40,14 +38,19 @@ public class ReadXml {
 				ZipEntry entry = (ZipEntry) entries.nextElement();
 				String entryName = entry.getName();
 				// We are dealing with a XML file
-				if (entryName.matches(".*\\.xml") && !entryName.equals("AndroidManifest.xml")) {
-//					System.out.println("Entry: " + entryName.substring(entryName.lastIndexOf("/")+1,entryName.lastIndexOf(".xml")));
-					String fileIdentifier = entryName.substring(entryName.lastIndexOf("/")+1,entryName.lastIndexOf(".xml"));
+				if (entryName.matches(".*\\.xml")
+						&& !entryName.equals("AndroidManifest.xml")) {
+					// System.out.println("Entry: " +
+					// entryName.substring(entryName.lastIndexOf("/")+1,entryName.lastIndexOf(".xml")));
+					String fileIdentifier = entryName.substring(
+							entryName.lastIndexOf("/") + 1,
+							entryName.lastIndexOf(".xml"));
 					xmlFile = archive.getInputStream(entry);
 					List<String> additionalEntryPointsMethodName = handleXmlFile(xmlFile);
 					xmlFile.close();
 
-					additionalEntryPoints.put(fileIdentifier, additionalEntryPointsMethodName);
+					additionalEntryPoints.put(fileIdentifier,
+							additionalEntryPointsMethodName);
 
 				}
 			}
@@ -61,36 +64,36 @@ public class ReadXml {
 		}
 
 	}
-	
+
 	/**
-	 *
-	 * @param classes all classes that exist in the Manifest file
+	 * 
+	 * @param classes
+	 *            all classes that exist in the Manifest file
 	 * @return
 	 */
-	public List<String> getAdditionalEntryPoints(Set<String> classes){
+	public List<String> getAdditionalEntryPoints(Set<String> classes) {
 		List<String> returnList = new ArrayList<String>();
-		
-		for(String set : classes){
-			for ( Map.Entry<String, List<String>> elem : additionalEntryPoints.entrySet() ){
-				for(String method : elem.getValue()){
-					returnList.add("<"+set+": void "+method + "(android.view.View)>");
+
+		for (String set : classes) {
+			for (Map.Entry<String, List<String>> elem : additionalEntryPoints
+					.entrySet()) {
+				for (String method : elem.getValue()) {
+					returnList.add("<" + set + ": void " + method
+							+ "(android.view.View)>");
 				}
 			}
 		}
-			  
-		
-		
+
 		return returnList;
 	}
 
-	private List<String> handleXmlFile(InputStream xmlFile) throws XmlPullParserException, IOException {
+	private List<String> handleXmlFile(InputStream xmlFile)
+			throws XmlPullParserException, IOException {
 		List<String> additionalEntryPointsMethodName = new ArrayList<String>();
 		// process xml file
 
 		AXmlResourceParser parser = new AXmlResourceParser();
 		parser.open(xmlFile);
-		
-		
 
 		String attribute;
 		while (true) {
@@ -104,20 +107,24 @@ public class ReadXml {
 			}
 			case XmlPullParser.START_TAG: {
 
-				String tagName = parser.getName();
+//				String tagName = parser.getName();
 				
-				//more listeners can be added likewise
-				if (tagName.equals("Button") || tagName.equals("RadioButton") || tagName.equals("ToggleButton") || tagName.equals("CheckBox")) {
-					
-					for(int i=0; i < parser.getAttributeCount(); i++){
+//				// more listeners can be added likewise
+//				if (tagName.equals("Button") || tagName.equals("RadioButton")
+//						|| tagName.equals("ToggleButton")
+//						|| tagName.equals("CheckBox")
+//						|| tagName.equals("ImageButton")){
+
+					for (int i = 0; i < parser.getAttributeCount(); i++) {
 						attribute = parser.getAttributeName(i);
-						if(attribute.equals("onClick")){
-							additionalEntryPointsMethodName.add(AXMLPrinter.getAttributeValue(parser,	i));
-//							System.out.println(AXMLPrinter.getAttributeValue(parser,i));
+						if (attribute.equals("onClick")) {
+							additionalEntryPointsMethodName.add(AXMLPrinter
+									.getAttributeValue(parser, i));
+							// System.out.println(AXMLPrinter.getAttributeValue(parser,i));
 						}
 					}
 
-				}
+//				}
 
 				break;
 			}
@@ -128,10 +135,8 @@ public class ReadXml {
 			}
 		}
 		parser.close();
-		
+
 		return additionalEntryPointsMethodName;
 
-		
 	}
-
 }
