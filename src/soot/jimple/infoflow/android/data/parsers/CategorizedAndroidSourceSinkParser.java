9bcb40d32e3f53a754c55da2634a23b75df01b73
Steven Arzt <Steven.Arzt@cased.de>
2013-10-04 17:41:24 +0200

    fixed a Java warning

--- src/soot/jimple/infoflow/android/data/parsers/CategorizedAndroidSourceSinkParser.java
+++ src/soot/jimple/infoflow/android/data/parsers/CategorizedAndroidSourceSinkParser.java
@@ -15,7 +15,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
