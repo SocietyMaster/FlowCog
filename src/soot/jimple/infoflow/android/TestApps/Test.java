bbfd3b9894f60c7824b101071c3cccecabfa4767
Steven Arzt <Steven.Arzt@cased.de>
2015-05-04 18:03:29 +0200

    change to match reflections in soot-infoflow

--- src/soot/jimple/infoflow/android/TestApps/Test.java
+++ src/soot/jimple/infoflow/android/TestApps/Test.java
@@ -39,7 +39,7 @@ import soot.jimple.infoflow.ipc.IIPCManager;
 import soot.jimple.infoflow.results.InfoflowResults;
 import soot.jimple.infoflow.results.ResultSinkInfo;
 import soot.jimple.infoflow.results.ResultSourceInfo;
-import soot.jimple.infoflow.solver.IInfoflowCFG;
+import soot.jimple.infoflow.solver.cfg.IInfoflowCFG;
 import soot.jimple.infoflow.taintWrappers.EasyTaintWrapper;
 import soot.jimple.infoflow.taintWrappers.ITaintPropagationWrapper;
 import soot.jimple.infoflow.taintWrappers.TaintWrapperSet;
