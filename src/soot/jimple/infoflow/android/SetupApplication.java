5711b95a5c2bce5ce1a43e68e9c70f7433206773
denis-bogdanas <denis.bogdanas@gmail.com>
2016-03-23 17:30:42 -0700

    #DP-60 Add an option to disable taint analysis for performance. Done.

--- src/soot/jimple/infoflow/android/SetupApplication.java
+++ src/soot/jimple/infoflow/android/SetupApplication.java
@@ -645,6 +645,7 @@ public class SetupApplication {
 				: Scene.v().getAndroidJarPath(androidJar, apkFileLocation);
 		if (this.additionalClasspath != null && !this.additionalClasspath.isEmpty())
 			classpath += File.pathSeparator + this.additionalClasspath;
+`		logger.debug("soot classpath: " + classpath);
 		return classpath;
 	}
 
